3680268f148c29033114b2c9e6ae7eccf225bea5
Niu-Li <13156361253@163.com>
2019-07-29 12:10:08 +0800

    Add FM25CL64 Driver!

--- bsp/stm32/stm32f107-uc-eval/applications/FM25CL64.c
+++ bsp/stm32/stm32f107-uc-eval/applications/FM25CL64.c
@@ -4,6 +4,56 @@
 #include <fm25cl64.h>
 #include <drv_spi.h>
 
+struct rt_spi_device *fm; 
+
+rt_err_t fm25cl64_RDSR(void *buffer)//¶Á FM25CL64 ×´Ì¬¼Ä´æÆ÷
+{
+	rt_uint8_t cmd = RDSR;
+	rt_spi_send_then_recv(fm,&cmd,1,buffer,1);
+	return RT_EOK;
+}
+
+rt_err_t fm25cl64_WRSR(void *buffer)//Ð´ FM25CL64 ×´Ì¬¼Ä´æÆ÷
+{
+	rt_uint8_t cmd = WREN;
+	rt_spi_send(fm,&cmd,1);
+	cmd = WRSR;
+	rt_spi_send_then_send(fm,&cmd,1,buffer,1);
+	cmd = WRDI;
+	rt_spi_send(fm,&cmd,1);
+	return RT_EOK;
+}
+
+rt_err_t fm25cl64_MemoryRead(rt_uint16_t addr,rt_uint8_t *data,rt_size_t length)
+{
+	rt_uint8_t cmd[3] = {READ,addr>>8,addr&0x0f};
+	rt_spi_send_then_recv(fm,cmd,3,data,length);
+	return RT_EOK;
+}
+
+rt_err_t fm25cl64_MemoryWrite(rt_uint16_t addr,rt_uint8_t *data,rt_size_t length)
+{
+	rt_uint8_t cmd[5] = {WREN,WRITE,addr>>8,addr&0x0f,WRDI};
+	rt_spi_send(fm,&cmd[0],1);
+	rt_spi_send_then_send(fm,&cmd[1],3,data,length);
+	rt_spi_send(fm,&cmd[4],1);
+	return RT_EOK;
+}
+
+
+static void fm25cl64_entry(void *parameter)
+{
+	rt_uint8_t buffer[8] = {0};
+	float t[2] = {12.3};
+	
+	while(1)
+	{
+		fm25cl64_RDSR(buffer);
+		
+		rt_kprintf("%d\n",buffer[0]);
+		rt_thread_delay(1000);
+	}
+}
 
 int fm25cl64_init()
 {
@@ -12,7 +62,35 @@ int fm25cl64_init()
 	
 	rt_hw_spi_device_attach("spi2","fm25cl64",GPIOB,GPIO_PIN_12);
 	
+	fm = rt_calloc(1,sizeof(struct rt_spi_device));
 	
+	fm = (struct rt_spi_device *)rt_device_find("fm25cl64");
+	if(fm==RT_NULL)
+		rt_kprintf("fm25cl64 find error!\n");
+	
+	{
+		struct rt_spi_configuration cfg;
+		cfg.data_width = 8;
+		cfg.max_hz = 18*1000*1000;
+		cfg.mode = RT_SPI_MASTER|RT_SPI_MODE_0|RT_SPI_MSB;
+		
+		rt_spi_configure(fm,&cfg);
+	}
+	
+	{
+		rt_thread_t tid;
+		tid = rt_thread_create("fm25cl64",fm25cl64_entry,RT_NULL,1024,8,20);
+		if(tid != RT_NULL)
+			rt_thread_startup(tid);
+		else 
+			rt_kprintf("fm25cl64 task create error!\n");
+	}
 	return 0;
 }
 INIT_DEVICE_EXPORT(fm25cl64_init);
+
+void fm25cl64_deinit(struct rt_spi_device *dev)
+{
+	RT_ASSERT(dev!=RT_NULL);
+	rt_free(dev);
+}
--- bsp/stm32/stm32f107-uc-eval/applications/FM25CL64.h
+++ bsp/stm32/stm32f107-uc-eval/applications/FM25CL64.h
@@ -3,6 +3,21 @@
 
 #include <board.h>
 
+#define WREN	0x06	//Ğ´Ê¹ÄÜÃüÁî
+#define WRDI	0x04	//Ğ´Ê§ÄÜÃüÁî
+#define RDSR	0x05	//¶Á×´Ì¬¼Ä´æÆ÷ÃüÁî
+#define WRSR	0x01	//Ğ´×´Ì¬¼Ä´æÆ÷ÃüÁî
+#define READ	0x03	//¶Á´æ´¢ÇøÃüÁî
+#define WRITE	0x02	//Ğ´´æ´¢ÇøÃüÁî
 
+
+
+rt_err_t fm25cl64_RDSR(void *buffer);
+rt_err_t fm25cl64_WRSR(void *buffer);
+rt_err_t fm25cl64_MemoryRead(rt_uint16_t addr,rt_uint8_t *data,rt_size_t length);
+rt_err_t fm25cl64_MemoryWrite(rt_uint16_t addr,rt_uint8_t *data,rt_size_t length);
+
+
+void fm25cl64_deinit(struct rt_spi_device *dev);
 
 #endif //fm25cl64.h
