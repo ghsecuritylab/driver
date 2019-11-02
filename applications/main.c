0c82e289c40af1a10f9d5816f82872290032024f
Niu-Li <13156361253@163.com>
2019-11-02 16:30:17 +0800

    在main函数添加SFUD探测W25Q256的函数并导出。

--- applications/main.c
+++ applications/main.c
@@ -27,6 +27,27 @@ int main(void)
         rt_thread_mdelay(1000);
         rt_pin_write(LCD_BLK, PIN_LOW);
         rt_thread_mdelay(1000);
+		rt_kprintf("test per 5s!\n");
+		rt_thread_mdelay(3000);
     }
     return RT_EOK;
 }
+
+#include "spi_flash.h"
+#include "spi_flash_sfud.h"
+#include "drv_spi.h"
+
+void flash_test()
+{
+	 __HAL_RCC_GPIOE_CLK_ENABLE();
+    rt_hw_spi_device_attach("spi4", "spi40", GPIOE, GPIO_PIN_4);
+
+    if (RT_NULL == rt_sfud_flash_probe("W25Q256", "spi40"))
+    {
+        rt_kprintf("sfud to W25Q256 failed!\n");
+		return;
+    }
+	rt_kprintf("sufd to W25Q256 sucessful!\n");
+    return ;
+}
+MSH_CMD_EXPORT(flash_test, W25Q256 device test);
